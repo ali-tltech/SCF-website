f1f1c13ccc072291ef864b8cf106b8a1269dbdc2
ali-tltech <murthasaali.tltechnologies@gmail.com>
2024-10-23 11:25:49 +0530

    Updated Ourstory section in landing page

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import SolutionsSec from "@/components/SolutionsSec";
 import WhatWeDo from "@/components/Home/WhatWeDo";
 import TestimonialCarousel from "@/components/Home/Testimonials";
 import OurStory from "@/components/Home/OurStory";
+import DiscoverSection from "@/components/Home/DiscoverSection";
 
 
   // <======================================== NOTES START ==============================================>
@@ -114,40 +115,7 @@ export default function Home() {
 
       {/* New Section (What We Do) */}
      <WhatWeDo/>
-        <div className="images-section relative md:px-20  grid grid-cols-1 md:grid-cols-2 gap-4 min-  p-8" style={{backgroundImage:"linear-gradient(to right top, #6ba6f2, #36b8f9, #00c9f7, #00d7eb, #34e3da, #45e9cf, #5ceec2, #75f3b4, #6df5aa, #66f7a0, #62f994, #5ffb87)"}}>
-        <div className="absolute inset-0 bg-black opacity-20"></div>
-          <div className="relative flex justify-center items-center h-[500px]">
-            <div className="absolute top-0 left-0 transform translate-x-4 translate-y-4">
-            <Image
-              src="/images/LandingPage/discover-2.png" // Ensure you provide a valid src
-              alt="Image 2"
-              width={500} // Add width
-              height={300} // Add height
-              className="h-1/2 w-auto "
-            />
-            </div>
-            <div className="absolute bottom-0 right-0 transform -translate-x-4 -translate-y-4" style={{ backgroundImage: "url(/images/LandingPage/discover-2.png)" }}>
-              <Image
-                src="/images/LandingPage/discover.jpg"
-                alt="Image 1"
-                width={500} // Add width
-                height={300} // Add height
-                className="h-1/2 w-auto "
-              />
-            </div>
-          </div>
-
-          <div className="flex z-20 flex-col justify-center items-start text-white p-6">
-            <h2 className="text-3xl font-bold mb-4">Engaging New Audiences through Smart Approach</h2>
-            <p className="mb-6">
-              There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
-              If you are going to use a passage.
-            </p>
-            <button className="bg-white text-teal-500 font-semibold px-6 py-3 rounded-lg shadow hover:bg-gray-100 transition">
-              DISCOVER MORE
-            </button>
-          </div>
-        </div>
+       <DiscoverSection/>
         // <StatsSection/>
         <IndustriesSection/>
         <RecentStories/>
