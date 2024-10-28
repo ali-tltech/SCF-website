f7371ce89f6663471ca57434be9b48cc15f684f5
guru-das-prr <gurudaspr.tltechnologies@gmail.com>
2024-10-28 09:15:05 +0530

    update contact page , update google map link

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useForm, Controller, set } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import PhoneInput from 'react-phone-input-2';
@@ -27,7 +27,6 @@ const ContactForm = () => {
             console.log('Form submitted successfully');
             toast.success('Message sent successfully!');
             setLoading(false);
-
             reset();
         } catch (error) {
             console.error('An error occurred', error);
@@ -35,7 +34,8 @@ const ContactForm = () => {
             setLoading(false);
         }
     };
-    const inputStyle = `w-full px-4 py-3 border rounded-lg transition-colors  text-black outline-none font-outfit text-sm`;
+
+    const inputStyle = `w-full px-4 py-3 border rounded-lg transition-colors text-black outline-none font-outfit text-base placeholder:text-base`;
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
@@ -45,7 +45,7 @@ const ContactForm = () => {
                         type="text"
                         placeholder="Enter your name"
                         {...register('name')}
-                        className={`${inputStyle} 'border-gray-300'}`}
+                        className={`${inputStyle} border-gray-300`}
                     />
                     <div className='absolute mr-2'>
                         {errors.name && <p className="text-red-500 text-xs ml-2">{errors.name.message}</p>}
@@ -58,21 +58,32 @@ const ContactForm = () => {
                         control={control}
                         render={({ field: { onChange, value } }) => (
                             <PhoneInput
-                                
+                                country={'us'}
                                 value={value || ''}
                                 onChange={onChange}
                                 countryCodeEditable={true}
                                 inputProps={{
                                     name: 'phone',
                                     required: true,
-                                    className: `${inputStyle}'border-gray-300 pl-10 text-black'}`,
+                                    className: `${inputStyle} border-gray-300 pl-10 text-black`,
+                                    style: {
+                                        fontSize: '1rem', // 16px
+                                        fontFamily: 'outfit, sans-serif'
+                                    }
                                 }}
-                                containerStyle={{ width: '100%' ,color: 'black' }}
-                                buttonStyle={{ borderTopLeftRadius: '0.5rem', borderBottomLeftRadius: '0.5rem' }}
+                                containerStyle={{ width: '100%', color: 'black' }}
+                                buttonStyle={{ 
+                                    borderTopLeftRadius: '0.5rem', 
+                                    borderBottomLeftRadius: '0.5rem',
+                                }}
+                                inputStyle={{
+                                    fontSize: '1rem',
+                                    width: '100%',
+                                }}
                             />
                         )}
                     />
-                    <div className='absolute '>
+                    <div className='absolute'>
                         {errors.phoneNumber && <p className="text-red-500 text-xs ml-2">{errors.phoneNumber.message}</p>}
                     </div>
                 </div>
@@ -83,7 +94,7 @@ const ContactForm = () => {
                     type="email"
                     placeholder="Enter your email"
                     {...register('email')}
-                    className={`${inputStyle}border-gray-300`}
+                    className={`${inputStyle} border-gray-300`}
                 />
                 <div className="absolute">
                     {errors.email && <p className="text-red-500 text-xs ml-2">{errors.email.message}</p>}
@@ -95,7 +106,7 @@ const ContactForm = () => {
                     placeholder="How can we help?"
                     rows={4}
                     {...register('message')}
-                    className={`${inputStyle}border-gray-300`}
+                    className={`${inputStyle} border-gray-300`}
                 ></textarea>
                 <div className="absolute">
                     {errors.message && <p className="text-red-500 text-xs ml-2">{errors.message.message}</p>}
@@ -104,19 +115,11 @@ const ContactForm = () => {
 
             <button
                 type="submit"
-                className="w-full bg-blue-500 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 flex items-center justify-center font-outfit text-sm"
+                className="w-full bg-blue-500 text-white font-semibold px-6 py-3 rounded-lg hover:bg-blue-600 transition duration-300 flex items-center justify-center font-outfit text-base"
             >
                 {loading ? 'Submitting...' : 'Contact Us Today'}
                 <span className="ml-2">➡️</span>
             </button>
-
-            {/* {submitStatus === 'success' && (
-                <div className="text-green-600 font-semibold">Form submitted successfully!</div>
-            )}
-            {submitStatus === 'error' && (
-                <div className="text-red-600 font-semibold">An error occurred. Please try again.</div>
-            )} */}
-            
         </form>
     );
 };
