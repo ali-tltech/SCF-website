d38d87da2ec53a03992aecfd7d8c11009f3e81d6
ali-tltech <murthasaali.tltechnologies@gmail.com>
2024-11-01 00:01:53 +0530

    Footer Padding Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-gray-300 ">
       {/* Main Footer Content */}
-      <div className="container mx-auto px-4 max-w-7xl md:py-10">
+      <div className="container mx-auto px-4 max-w-7xl py-4 md:py-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Quick Links Section */}
           <div className="space-y-4">
